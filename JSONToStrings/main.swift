3dcf028d3019131e2e0bfc2ad8e0c905573fd9ec
Csaba Gyarmati <csaba.gyarmati@icellmobilsoft.hu>
2020-08-18 10:38:16 +0200

    Add customized help and errors for JSONToStrings

--- JSONToStrings/main.swift
+++ JSONToStrings/main.swift
@@ -9,16 +9,18 @@
 import Foundation
 import ArgumentParser
 
-enum JsonToStringsError: Error {
-    case fileNotExists(fileName: String)
-    case notADirectory(fileSpec: String)
-    case localizableJsonNotFound(fileSpec: String)
-    case localizableJsonCannotBeRead(fileSpec: String)
-    case serializationError(fileSpec: String)
+struct RuntimeError: Error, CustomStringConvertible {
+    var description: String
+    
+    init(_ description: String) {
+        self.description = description
+    }
 }
 
 struct JsonToStrings: ParsableCommand {
-    @Option(help: "Root path of project Localized.strings generating for.")
+    static var configuration = CommandConfiguration(commandName: "JSONToStrings")
+    
+    @Option(name: .shortAndLong, help: "Path of Localizable.json and target path of Localizable.strings files.")
     var path: String
 
     func run() throws {
@@ -32,23 +34,23 @@ struct JsonToStrings: ParsableCommand {
         let fileManager = FileManager.default
         var isDir: ObjCBool = false
         if !fileManager.fileExists(atPath: dirURL.path, isDirectory: &isDir) {
-            throw JsonToStringsError.fileNotExists(fileName: dirURL.path)
+            throw RuntimeError("Path not exists: \(dirURL.path)")
         }
         if !isDir.boolValue {
-            throw JsonToStringsError.notADirectory(fileSpec: dirURL.path)
+            throw RuntimeError("Provided path is not a folder: \(dirURL.path)")
         }
 
         let fileURL = dirURL.appendingPathComponent("Localizable.json")
         if !fileManager.fileExists(atPath: fileURL.path) {
-            throw JsonToStringsError.localizableJsonNotFound(fileSpec: fileURL.path)
+            throw RuntimeError("Localizable.json not found at path: \(fileURL.path)")
         }
 
         guard let jsonData = try? Data(contentsOf: fileURL) else {
-            throw JsonToStringsError.localizableJsonCannotBeRead(fileSpec: fileURL.path)
+            throw RuntimeError("Couldn't read Localizable.json file.")
         }
 
         guard let json = try? JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] else {
-            throw JsonToStringsError.serializationError(fileSpec: fileURL.path)
+            throw RuntimeError("Serialization error at processing Localizable.json")
         }
         
         return GeneratorArguments(json: json, dirUrl: dirURL)
